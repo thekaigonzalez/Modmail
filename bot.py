09e81d503ce5b97280cf835286953a25e402e79c
kai Gonzalez <gkai70264@gmail.com>
2022-03-24 17:54:08 -0400

    Add UDIM config (1.0.1)

--- bot.py
+++ bot.py
@@ -1,13 +1,32 @@
+import json
 import os
+import pathlib
 import discord
+import toml
 from discord.ext import commands
 
-client = commands.Bot(command_prefix="-")
+EXCLUDE_AFTER_USE = False
+SAVE_VALUES_ON_EXIT = False
+if pathlib.Path("udim.toml").exists():
+    t = toml.load('udim.toml')
+    EXCLUDE_AFTER_USE = t['admin_settings']["excludeAfterUse"]
+class Bot(commands.Bot):
+    async def async_cleanup(self):  # example cleanup function
+        print("Exiting...")
+            
+    async def close(self):
+        # do your cleanup here
+        await self.async_cleanup()
+        
+        await super().close()  # don't forget this!
+
+client = Bot(command_prefix="-")
 
 @client.event
 async def on_ready():
     users = {}
     extras = {}
+    exclude = []
 
 def find_user_from_users(id):
     for m in users:
@@ -25,6 +44,7 @@ def get_token_channel_id(id):
 
 @client.event
 async def on_message(msg: discord.Message):
+    global exclude
     global user
     global extras
     global users
@@ -38,6 +58,12 @@ async def on_message(msg: discord.Message):
             extras
     except Exception:
             extras = {}
+
+    try:
+            exclude
+    except Exception:
+            print("reset")
+            exclude = []
    
     if msg.author == client.user: return
     if msg.content.startswith("-"):
@@ -66,17 +92,24 @@ async def on_message(msg: discord.Message):
             
             tokchan = msg.guild
         if args[0] == 'close':
+            exclude.append(args[1])
             if get_token_channel_id(args[1]) != None:
+                
                 await msg.channel.delete()
                 del extras[args[1]]
                 del users[msg.channel.id]
+                
+
 
     f,i = find_user_from_users(str(msg.author.id))
     if not msg.guild and get_token_channel_id(str(msg.author.id)):
         await get_token_channel_id(str(msg.author.id)).send(msg.content)
     
     if not msg.guild and extras.get(str(msg.author.id)) == None:
-        
+        if str(msg.author.id) in exclude:
+            print("Excluded")
+            await msg.channel.send("You can not send another modmail. `EXCLUDE_AFTER_CLOSE` is enabled.")
+            return
         await tokchan.create_text_channel(str(msg.author.id) + "-token")
         ch = None
 
